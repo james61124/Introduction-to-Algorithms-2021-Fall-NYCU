86c7b4b0085068b92e6ba64bd6516a3fce3ea423
unknown <james61124@gmail.com>
2022-12-07 20:24:24 +0800

    final

---
+++ Lab0-Hello World!/Lab0 - Hello World!.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	if (n == 1) {
+		cout << "Hello World!" << endl;
+	}
+	else if(n==2){
+		cout << "nycualgo.kcw@gmail.com" << endl;
+	}
+	
+}
